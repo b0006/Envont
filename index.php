f51f78906dd3183a2248e2d5e8a2e0579fc54f63
Alexandr Kasikov <qsefthuken@gmail.com>
2018-07-02 13:22:02 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Envato Bangladesh</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100|Roboto+Slab:100|Merriweather+Sans" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap_grid.css">
+
+</head>
+
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="logo">
+                        <div class="img__logo"></div>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="top-menu">
+                        <ul>
+                            <li class="top-menu__item"><a class="top-menu__link top-menu__link_active" href="#hours">Event Details</a></li>
+                            <li class="top-menu__item"><a class="top-menu__link" href="#speakers">Speakers</a></li>
+                            <li class="top-menu__item"><a class="top-menu__link" href="#sponsors">Sponsors</a></li>
+                            <li class="top-menu__item"><a class="top-menu__link" href="#past_events">Past Events</a></li>
+                            <li class="top-menu__item"><a class="top-menu__link" href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section id="hours" class="hours">
+        <div class="container">
+            <div class="content">
+                <div class="content__values row">
+                    <div class="col-md-3">
+                        <span class="content__days">0</span>
+                        <span class="content__colon">:</span>
+                    </div>
+                    <div class="col-md-3">
+                        <span class="content__hours">0</span>
+                        <span class="content__colon">:</span>
+                    </div>
+                    <div class="col-md-3">
+                        <span class="content__minutes">0</span>
+                        <span class="content__colon">:</span>
+                    </div>
+                    <div class="col-md-3">
+                        <span class="content__seconds">0</span>
+                    </div>
+                </div>
+
+                <div class="content__description row">
+                    <div class="col-md-3">
+                        Days
+                    </div>
+                    <div class="col-md-3">
+                        Hours
+                    </div>
+                    <div class="col-md-3">
+                        Minutes
+                    </div>
+                    <div class="col-md-3">
+                        Seconds
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sing-up">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="icons">
+                        <img class="icons__img" src="assets/img/icons.png">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form">
+                        <h2 class="form__title">Sign up for our event</h2>
+                        <form action="/assets/php/mail.php" method="post">
+                            <div class="row">
+                                <input class="form__input" type="text" name="user_name" placeholder="Enter your name">
+                            </div>
+                            <div class="row">
+                                <input class="form__input" type="email" name="user_email" placeholder="Enter your email">
+                            </div>
+                            <div class="row">
+                                <input id="phone-mask" class="form__input" type="text" name="user_phone" placeholder="Enter your phone">
+                            </div>
+                            <div class="row">
+                                <input class="form__input" type="text" name="user_login" placeholder="Enter your Envato login">
+                            </div>
+                            <div class="row">
+                                <input class="form__button" type="submit" name="submit" value="Let me in!">
+                            </div>
+                        </form>
+                        <div class="form__hurry-up">
+                            Hurry up, Limited seats availble
+                                *we will never spam your email
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="meetup">
+        <div class="container">
+            <div class="row">
+                <div class="welcome">
+                    <div class="col-md-7">
+                        <h2 class="welcome__title">Welcome!</h2>
+                        <p class="welcome__paragraph">
+                            The biggest envato community meetup in Bangladesh is happening right now! And you can be a part of it too...
+                        </p>
+                        <p class="welcome__paragraph">
+                            Get together with members of the Envato community to talk shop, trade war stories, make new friends and see the faces behind the avatars. Whether you’re an author on Envato Market, a client on Envato Studio, or a learner on Tuts+, you are invited!
+                        </p>
+                        <p class="welcome__paragraph">
+                            It doesn’t matter as long as you have the motivation and interest to work at the envato and bring cookies to our event. Just kidding, but we don’t mind having more cookies! ;)Head over and fill up the sign up form here so that we can keep track of our guests. Don’t forget to register on our nvite page here too!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="community">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <img class="community__img" src="assets/img/community.png" alt="community" >
+                    <h2 class="community__title">Community</h2>
+                    <div class="community__desc">Meet the Community you’ve always talked with, in real life! This meetup will be all about authors engaging each other with interesting coversation and topics. We will have loads of fun.</div>
+                </div>
+                <div class="col-md-4">
+                    <img class="community__img" src="assets/img/task_shop.png" alt="task_shop" >
+                    <h2 class="community__title">Task Shop</h2>
+                    <div class="community__desc">In this meetup you will get to know the marketplaces better, because that’s why were all here for right? Learn a few tips and tricks from experienced authors from all over the country.</div>
+                </div>
+                <div class="col-md-4">
+                    <img class="community__img" src="assets/img/war_stories.png" alt="war-stories">
+                    <h2 class="community__title">War Stories</h2>
+                    <div class="community__desc">Gather round fellow authors!Hear the stories of success from your favorite authors and learn how they we able to tackle their problems and become successful.</div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="time">
+        <div class="container">
+            <table class="time__table">
+                <thead class="time__thead">
+                <tr class="time__tr"><th class="time__th-thead">Film<th class="time__th-thead">Stuff that will happen
+                <tbody class="time__tbody">
+                <tr class="time__tr">
+                    <td class="time__td">10am - 12pm
+                    <td class="time__td">Introduction and presentations about Envatods
+                <tr class="time__tr">
+                    <td class="time__td">12pm - 02pm
+                    <td class="time__td">Sessions by honorable guests and Speakers
+                <tr class="time__tr">
+                    <td class="time__td">02pm - 03pm
+                    <td class="time__td">Break and Mingle time with the authors
+                <tr class="time__tr">
+                    <td class="time__td">03pm - 05pm
+                    <td class="time__td">Portfolio Review, Rewards and Ending Presentation
+            </table>
+        </div>
+    </section>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
+
+<script>
+
+</script>
+
+<script src="https://unpkg.com/imask"></script>
+
+<script src="assets/js/timer.js"></script>
+<script src="assets/js/mask.js"></script>
+
+<script>
+    var headertext = [],
+        headers = document.querySelectorAll("#miyazaki th"),
+        tablerows = document.querySelectorAll("#miyazaki th"),
+        tablebody = document.querySelector("#miyazaki tbody");
+
+    for(var i = 0; i < headers.length; i++) {
+        var current = headers[i];
+        headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+    }
+    for (var i = 0, row; row = tablebody.rows[i]; i++) {
+        for (var j = 0, col; col = row.cells[j]; j++) {
+            col.setAttribute("data-th", headertext[j]);
+        }
+    }
+</script>
